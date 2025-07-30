a0a7e78fb34a138a0224058f0ade06d15aa896f2
RHUANPR <rhuanprimo5@gmail.com>
2025-07-30 16:46:45 -0300

    colocando cor laranja

--- constants.ts
+++ constants.ts
@@ -1,14 +1,15 @@
 
+
 import { Product, Theme } from './types';
 
-export const ADMIN_PASSWORD = 'Rhuan123'; // For demonstration purposes
+export const ADMIN_PASSWORD = 'Rhuan123';
 
 export const INITIAL_PRODUCTS: Product[] = [];
 
 export const INITIAL_THEME: Theme = {
-  primary: '#2563eb', // blue-600
+  primary: '#E0872E',
   secondary: '#475569', // slate-600
-  accent: '#ef4444', // red-500
+  accent: '#E0872E',
   base100: '#f8fafc', // slate-50
   base200: '#e2e8f0', // slate-200
   base300: '#cbd5e1', // slate-300
